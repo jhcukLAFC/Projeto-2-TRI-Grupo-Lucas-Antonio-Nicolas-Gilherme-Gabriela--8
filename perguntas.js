4f94905b0319ef0015af3f0524b98a1ff80393ab
jhcukLAFC <castilhos.lucas@escola.pr.gov.br>
2025-08-06 05:20:51 +0000

    aula8

---
+++ perguntas.js
@@ -0,0 +1,32 @@
+criaCartao(
+"Estados",
+"Quais são os estados da região Sul?  " ,
+"Paraná, Santa Catarina e Rio Grande do Sul"
+)
+        
+criaCartao(
+"Estados",
+"Quais são os estados da região Sudeste?",
+"São Paulo, Rio de Janriro, Espírito Santo e Minas Gerais"
+)
+
+criaCartao(
+"Estados",
+"Quais são os estados da região Centro-Oeste?",
+"Goias, Mato Grosso e Mato Grosso do Sul"
+)
+
+criaCartao(
+"    Estados",
+"Quais são os estados da região Nordeste?",
+"Bahia, Alagoas, Sergipe, Pernambuco, Paraiba, Rio Grande do Norte, Ceará, Piauí e Maranhão"
+
+)    
+
+criaCartao(
+"Estados",
+"Quais são os estados da região Norte?",
+"Acre, Amapá, Amazonas, Pará, Rondônia, Roraima e Tocantins"
+)
+
+        
